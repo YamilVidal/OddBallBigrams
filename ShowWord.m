896fcaa2310cdb04b7b8347cea29ef981241b6ee
YamilVidal <yvidal@sissa.it>
2017-09-13 20:26:03 +0200

    Preserve vertical size of characters.

--- ShowWord.m
+++ ShowWord.m
@@ -9,23 +9,23 @@ Words = E.Stim.Words;
 inx = (E.screen.res(1) - size(E.Stim.I{Words(w,1)},2) - size(E.Stim.I{Words(w,2)},2) - size(E.Stim.I{Words(w,3)},2))/2;
 
 pos = [inx;
-    cy - 125;
+    cy - size(E.Stim.I{Words(w,1)},1)/2;
     inx + size(E.Stim.I{Words(w,1)},2);
-    cy + 125];
+    cy + size(E.Stim.I{Words(w,1)},1)/2];
 
 Screen('DrawTexture', E.screen.theWindow, E.Stim.Char{Words(w,1)},[],pos);
 
 pos = [pos(3);
-    pos(2);
+    cy - size(E.Stim.I{Words(w,2)},1)/2;
     pos(3) + size(E.Stim.I{Words(w,2)},2);
-    pos(4)];
+    cy + size(E.Stim.I{Words(w,1)},1)/2];
 
 Screen('DrawTexture', E.screen.theWindow, E.Stim.Char{Words(w,2)},[],pos);
 
 pos = [pos(3);
-    pos(2);
+    cy - size(E.Stim.I{Words(w,3)},1)/2;
     pos(3) + size(E.Stim.I{Words(w,3)},2);
-    pos(4)];
+    cy + size(E.Stim.I{Words(w,1)},1)/2];
 
 Screen('DrawTexture', E.screen.theWindow, E.Stim.Char{Words(w,3)},[],pos);
 
