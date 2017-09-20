a3d9cc06b3a111fd9c964790aa2e47bb3d418fd3
YamilVidal <yvidal@sissa.it>
2017-09-20 16:38:58 +0200

    Final version of experiment

--- SetupTiming.m
+++ SetupTiming.m
@@ -1,12 +1,12 @@
 
-E.times.NBlocks = 8; % Number of Blocks
+E.times.NBlocks = 6; % Number of Blocks
 E.times.NWords  = 200; % Max is 200
 
 % E.times.PresT = 0.1; % time from the question to the onset of the test word
 E.times.RespT = 2.0; % time to give response
 E.times.SOA   = 0.5; % Actual SOA is this plus rand from 0 to .5. This in addition to RespT.
 
-E.times.Break     = 60*1.5; % Break between blocks
+E.times.Break     = 60*1; % Break between blocks
 E.times.CountDown = 10; % Break between blocks countdown
 
 E.times.BlockCounter = 1;
